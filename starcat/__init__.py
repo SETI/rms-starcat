e32c51a25683541b4a6255445e5eaf8e80eabef7
Robert French <rfrench@seti.org>
2025-01-24 12:59:37 -0800

    Minor fixes

--- starcat/__init__.py
+++ starcat/__init__.py
@@ -2,7 +2,10 @@
 # starcat/__init__.py
 ################################################################################
 
-from starcat.starcatalog import Star, StarCatalog  # noqa: F401
+from starcat.starcatalog import (Star,
+                                 StarCatalog,
+                                 SCLASS_TO_B_MINUS_V,
+                                 SCLASS_TO_SURFACE_TEMP)  # noqa: F401
 from starcat.spice import SpiceStar, SpiceStarCatalog  # noqa: F401
 from starcat.ucac4 import UCAC4Star, UCAC4StarCatalog  # noqa: F401
 from starcat.ybsc import YBSCStar, YBSCStarCatalog  # noqa: F401
@@ -14,6 +17,8 @@ except ImportError:
     __version__ = 'Version unspecified'
 
 __all__ = [
+    'SCLASS_TO_B_MINUS_V',
+    'SCLASS_TO_SURFACE_TEMP',
     'Star',
     'StarCatalog',
     'SpiceStar',
